4d5fae70a398a387f7164d6f5e6b697b895cfbaa
Arseyillion <echukwuemeka017@gmail.com>
2026-02-07 21:33:39 +0100

    this is the first veersion of strapi that is working withh strapi although its still having issues with working well with our frontend here locally

--- config/database.ts
+++ config/database.ts
@@ -24,11 +24,11 @@ export default ({ env }) => {
     },
     postgres: {
       connection: {
-        connectionString: env('DATABASE_URL'),
-        host: env('DATABASE_HOST', 'imasqptbfyyfqnaqixks.supabase.co'),
+        connectionString: env('DATABASE_URL', 'postgresql://postgres.imasqptbfyyfqnaqixks:RkH2r8YLhsgdL80Y@aws-1-us-east-2.pooler.supabase.com:6543/postgres'),
+        host: env('DATABASE_HOST', 'aws-1-us-east-2.pooler.supabase.com'),
         port: env.int('DATABASE_PORT', 5432),
         database: env('DATABASE_NAME', 'postgres'),
-        user: env('DATABASE_USERNAME', 'postgres'),
+        user: env('DATABASE_USERNAME', 'postgres.imasqptbfyyfqnaqixks'),
         password: env('DATABASE_PASSWORD', 'RkH2r8YLhsgdL80Y'),
         ssl: env.bool('DATABASE_SSL', true) && {
           key: env('DATABASE_SSL_KEY', undefined),
@@ -36,11 +36,11 @@ export default ({ env }) => {
           ca: env('DATABASE_SSL_CA', undefined),
           capath: env('DATABASE_SSL_CAPATH', undefined),
           cipher: env('DATABASE_SSL_CIPHER', undefined),
-          rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', true),
+          rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', false),
         },
         schema: env('DATABASE_SCHEMA', 'public'),
       },
-      pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10), acquireTimeoutMillis: 60000, createTimeoutMillis: 30000, destroyTimeoutMillis: 5000, idleTimeoutMillis: 30000 },
+      pool: { min: env.int('DATABASE_POOL_MIN', 0), max: env.int('DATABASE_POOL_MAX', 3), acquireTimeoutMillis: 60000, createTimeoutMillis: 30000, destroyTimeoutMillis: 5000, idleTimeoutMillis: 30000 },
     },
     sqlite: {
       connection: {
